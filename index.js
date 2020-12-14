69aaffc85a0f19d46ea6694da2b89578e7c1a91e
Daniel Fischer <daniel@d-fischer.dev>
2020-12-14 21:57:22 +0100

    add a bunch of stricter rules

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ module.exports = {
 	rules: {
 		'@typescript-eslint/adjacent-overload-signatures': 'error',
 		'@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+		'@typescript-eslint/await-thenable': 'error',
+		'@typescript-eslint/ban-ts-comment': [
+			'error',
+			{
+				'ts-expect-error': 'allow-with-description',
+				'ts-ignore': false,
+				'ts-nocheck': false,
+				'ts-check': true
+			}
+		],
+		'@typescript-eslint/ban-tslint-comment': 'error',
+		// uses defaults only
+		'@typescript-eslint/ban-types': 'error',
+		'@typescript-eslint/class-literal-property-style': ['error', 'fields'],
+		'@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
 		'@typescript-eslint/consistent-type-assertions': [
 			'error',
 			{
@@ -17,7 +32,14 @@ module.exports = {
 			}
 		],
 		'@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-		'@typescript-eslint/consistent-type-imports': 'error',
+		'@typescript-eslint/consistent-type-imports': [
+			'error',
+			{
+				prefer: 'type-imports',
+				disallowTypeAnnotations: true
+			}
+		],
+		'@typescript-eslint/dot-notation': 'error',
 		'@typescript-eslint/explicit-member-accessibility': [
 			'error',
 			{
@@ -35,6 +57,19 @@ module.exports = {
 				allowTypedFunctionExpressions: true
 			}
 		],
+		'@typescript-eslint/member-delimiter-style': [
+			'error',
+			{
+				multiline: {
+					delimiter: 'semi',
+					requireLast: true
+				},
+				singleline: {
+					delimiter: 'semi',
+					requireLast: false
+				}
+			}
+		],
 		'@typescript-eslint/member-ordering': [
 			'error',
 			{
@@ -49,11 +84,40 @@ module.exports = {
 				]
 			}
 		],
+		'@typescript-eslint/method-signature-style': ['error', 'property'],
+		'@typescript-eslint/no-base-to-string': 'error',
+		'@typescript-eslint/no-confusing-non-null-assertion': 'error',
+		'@typescript-eslint/no-confusing-void-expression': [
+			'error',
+			{
+				ignoreArrowShorthand: true
+			}
+		],
+		'@typescript-eslint/no-dynamic-delete': 'error',
+		'@typescript-eslint/no-dupe-class-members': 'error',
 		'@typescript-eslint/no-duplicate-imports': 'error',
+		'@typescript-eslint/no-empty-function': 'error',
 		'@typescript-eslint/no-empty-interface': 'error',
 		'@typescript-eslint/no-explicit-any': 'error',
+		'@typescript-eslint/no-extra-parens': [
+			'error',
+			'all',
+			{
+				conditionalAssign: false,
+				nestedBinaryExpressions: false
+			}
+		],
+		'@typescript-eslint/no-extra-non-null-assertion': 'error',
+		'@typescript-eslint/no-extra-semi': 'error',
 		'@typescript-eslint/no-extraneous-class': 'error',
+		'@typescript-eslint/no-floating-promises': [
+			'error',
+			{
+				ignoreVoid: true
+			}
+		],
 		'@typescript-eslint/no-for-in-array': 'error',
+		'@typescript-eslint/no-implied-eval': 'error',
 		'@typescript-eslint/no-inferrable-types': [
 			'error',
 			{
@@ -61,7 +125,12 @@ module.exports = {
 				ignoreParameters: true
 			}
 		],
+		'@typescript-eslint/no-invalid-this': 'error',
+		'@typescript-eslint/no-invalid-void-type': 'error',
+		'@typescript-eslint/no-loop-func': 'error',
+		'@typescript-eslint/no-loss-of-precision': 'error',
 		'@typescript-eslint/no-misused-new': 'error',
+		'@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
 		'@typescript-eslint/no-redeclare': 'error',
 		'@typescript-eslint/no-require-imports': 'error',
 		'@typescript-eslint/no-shadow': 'error',
@@ -71,17 +140,44 @@ module.exports = {
 				allowDestructuring: true
 			}
 		],
-		'@typescript-eslint/no-unnecessary-qualifier': 'error',
-		'@typescript-eslint/no-useless-constructor': 'error',
-		'@typescript-eslint/no-use-before-define': 'error',
-		'@typescript-eslint/prefer-function-type': 'error',
-		'@typescript-eslint/promise-function-async': [
+		'@typescript-eslint/no-throw-literal': 'error',
+		'@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+		'@typescript-eslint/no-unnecessary-condition': [
 			'error',
 			{
-				allowAny: true
+				allowConstantLoopConditions: true
 			}
 		],
+		'@typescript-eslint/no-unnecessary-qualifier': 'error',
+		'@typescript-eslint/no-unnecessary-type-arguments': 'error',
+		'@typescript-eslint/no-unnecessary-type-assertion': 'error',
+		'@typescript-eslint/no-unnecessary-type-constraint': 'error',
+		'@typescript-eslint/no-unsafe-assignment': 'error',
+		'@typescript-eslint/no-unsafe-call': 'error',
+		'@typescript-eslint/no-unsafe-member-access': 'error',
+		'@typescript-eslint/no-unsafe-return': 'error',
+		'@typescript-eslint/no-unused-expressions': 'error',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/no-use-before-define': 'error',
+		'@typescript-eslint/no-useless-constructor': 'error',
+		'@typescript-eslint/no-var-requires': 'error',
+		'@typescript-eslint/non-nullable-type-assertion-style': 'error',
+		'@typescript-eslint/prefer-as-const': 'error',
+		'@typescript-eslint/prefer-for-of': 'error',
+		'@typescript-eslint/prefer-function-type': 'error',
+		'@typescript-eslint/prefer-includes': 'error',
+		'@typescript-eslint/prefer-nullish-coalescing': 'error',
+		'@typescript-eslint/prefer-optional-chain': 'error',
+		'@typescript-eslint/prefer-readonly': 'error',
+		'@typescript-eslint/prefer-reduce-type-parameter': 'error',
+		'@typescript-eslint/prefer-regexp-exec': 'error',
+		'@typescript-eslint/prefer-string-starts-ends-with': 'error',
+		'@typescript-eslint/promise-function-async': 'error',
+		'@typescript-eslint/require-array-sort-compare': 'error',
 		'@typescript-eslint/restrict-plus-operands': 'error',
+		'@typescript-eslint/restrict-template-expressions': 'error',
+		'@typescript-eslint/return-await': ['error', 'in-try-catch'],
+		'@typescript-eslint/switch-exhaustiveness-check': 'error',
 		'@typescript-eslint/triple-slash-reference': [
 			'error',
 			{
@@ -90,8 +186,9 @@ module.exports = {
 				lib: 'always'
 			}
 		],
-		// lots of false positives right now
-		// '@typescript-eslint/unbound-method': 'error',
+		// lots of false positives right now?
+		'@typescript-eslint/unbound-method': 'error',
+		'@typescript-eslint/unified-signatures': 'error',
 		'arrow-body-style': ['error', 'as-needed'],
 		camelcase: 'off',
 		'@typescript-eslint/naming-convention': [
@@ -190,19 +287,8 @@ module.exports = {
 		'no-new': 'error',
 		'no-new-wrappers': 'error',
 		'no-prototype-builtins': 'error',
-		'no-restricted-syntax': [
-			'error',
-			{
-				selector:
-					'BinaryExpression[operator=/^[!=]==?$/][left.raw=/^(true|false)$/], BinaryExpression[operator=/^[!=]==?$/][right.raw=/^(true|false)$/]',
-				message: "Don't compare for equality against boolean literals"
-			},
-			{
-				selector: 'SequenceExpression',
-				message: 'The comma operator is confusing and a common mistake. Don’t use it!'
-			}
-		],
 		'no-return-await': 'error',
+		'no-sequences': 'error',
 		'no-sparse-arrays': 'error',
 		'no-throw-literal': 'error',
 		'no-unexpected-multiline': 'error',
